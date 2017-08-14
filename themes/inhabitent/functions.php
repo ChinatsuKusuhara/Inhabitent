7e0cf7a91669a9f6ebc4ff2a79a0a5046d8e65b3
Chinatsu <chinatsu.k@hotmail.com>
2017-08-14 16:01:02 -0700

    style about-herobanner

--- themes/inhabitent/functions.php
+++ themes/inhabitent/functions.php
@@ -84,7 +84,8 @@ add_filter( 'stylesheet_uri', 'red_starter_minified_css', 10, 2 );
  */
 function inhabitent_scripts() {
 	wp_enqueue_style( 'inhabitent-style', get_stylesheet_uri() );
-
+	wp_enqueue_style( 'tent-style', get_stylesheet_uri() );
+	
 	wp_enqueue_script( 'jquery' );
 	wp_enqueue_script('font-awesome', 'https://use.fontawesome.com/e28c7aff88.js', array(), '4.7.0', false); 
 	wp_enqueue_script( 'inhabitent-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20130115', true );
