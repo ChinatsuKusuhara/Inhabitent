6592b601efbd6c3d10e060fc9e7dd65aecf29cad
Chinatsu <chinatsu.k@hotmail.com>
2017-08-17 00:03:25 -0700

    keep working on fron-page, style journal

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -8,17 +8,15 @@
 get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
+		<div class="hero-banner">
+		<img class="logo-circle" src=" <?php echo get_template_directory_uri(); ?>/asset/logos/inhabitent-logo-full.svg" alt="Inhabitent Camping Supply Co.">
+	</div>
 			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
 
 			<?php endwhile; // End of the loop. ?>
 
 		</main><!-- #main -->
-	<div class="hero-banner">
-		<img class="logo-circle" src=" <?php echo get_template_directory_uri(); ?>/asset/logos/inhabitent-logo-full.svg" alt="Inhabitent Camping Supply Co.">
-	</div>
+
 	</div><!-- #primary -->
 
 	<?php
