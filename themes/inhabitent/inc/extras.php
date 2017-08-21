e02e8a414c38bf340059f76acb826bfe59f2d112
Chinatsu <chinatsu.k@hotmail.com>
2017-08-20 21:42:30 -0700

    keep typing Journal page

--- themes/inhabitent/inc/extras.php
+++ themes/inhabitent/inc/extras.php
@@ -108,41 +108,9 @@ function inhabitent_archive_title($title) {
 }
 add_filter ('get_the_archive_title', 'inhabitent_archive_title');
 
-function red_wp_trim_excerpt( $text ) {
-		$raw_excerpt = $text;
-		if ( '' == $text ) {
-
-			//for grabbing the post content
-			$text = get_the_content('');
-
-			//delete all shortcode tags from the content 
-			$text = strip_shortcodes( $text );
-			$text = apply_filters( 'the_content', $text );
-			$text = str_replace( ']]>', ']]&gt;', $text );
-
-			//indicate allowable tags
-			$allowed_tags == '<p>,<a>,<em>,<strong>,<blockquote>,<cite>';
-			$text = strip_tags( $text, $allowed_tags );
-
-			//change to desired word count 
-			$excerpt_word_count = 50;
-			$excerpt_length = apply_filters( 'excerpt_length', $excerpt_word_count ); 
-
-			//create custom "more" page 
-			$excerpt_end = '<span>[...]</span><p><a href="' . get_permalink() .' " class="read-more">Read More &rarr;</a></p>';
-			$excerpt_more = apply_filters( 'excerpt_more', ' ' . $excerpt_end);
-
-			//add the elipisis + link end if word count is over the limit 
-			$word = preg_split( "/[ \n\r\t ]+/", $text, $excerpt_length + 1, PREG_SPLIT_NO_EMPTY );
-			if ( count( $words ) > $excerpt_length ) {
-				array_pop( $words );
-				$text = implode( ' ', $words );
-				$text = $text . $excerpt_more;
-			} else {
-				$text = implode( ' ', $words );
-			}
-			}
-			return apply_filters( 'wp_trim_excerpt', $text, $raw_excerpt );		
-		}
-remove_filter( 'get_the_excerpt', 'wp_trim_excerpt');
-add_filter( 'get_the_excerpt', 'red_wp_trim_excerpt'); 
+// Replaces the excerpt "Read More" text by a link
+function new_excerpt_more($more) {
+	global $post;
+	return ' [...]<a class="moretag" href="'. get_permalink($post->ID) . '">Read More <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>';
+}
+add_filter('excerpt_more', 'new_excerpt_more');
